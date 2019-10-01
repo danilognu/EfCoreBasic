45d885424124eed35cf59394af35afe62870fed6
danilognu <30319489+danilognu@users.noreply.github.com>
2019-10-01 11:09:41 -0300

    Commit Pasta Domain
    Dominio

---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,17 @@
+namespace Domain.Entities
+{
+    public class Person
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;} 
+        public string Email {get; set;} 
+        public string Bairro {get; set;} 
+        public string Endereco {get; set;} 
+        public int Numero {get; set;}
+        public string Cep {get; set;}
+        public string Telefone1 {get; set;} 
+        public string Telefone2 {get; set;} 
+        public string Sexo {get; set;}
+        public string Status {get; set;} 
+    }
+}
